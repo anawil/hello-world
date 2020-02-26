7f77ef52e686e67628c9726ac87952e5042b0947
root <root@ip-172-31-22-6.us-east-2.compute.internal>
2020-02-26 08:31:59 +0000

    Prepr for heroku

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from flask import Flask, escape, request
 app = Flask(__name__)
 @app.route('/')
 def hello():
-        name = request.args.get("name", "World")
-            return f'Hello, {escape(name)}!'
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
